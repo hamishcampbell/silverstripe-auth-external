fd1bf8643b4335178907909a18086dd1154988a7
lancer <lancer@467b73ca-7a2a-4603-9d3b-597d59a354a9>
2007-11-12 20:56:52 +0000

    Initial commit

---
+++ lang/en_US.php
@@ -0,0 +1,18 @@
+<?php
+
+global $lang;
+
+$lang['en_US']['ExternalAuthenticator']['EnterUID'] = 'Please enter a %s';
+$lang['en_US']['ExternalAuthenticator']['Failed'] = 'Authentication failed';
+$lang['en_US']['ExternalAuthenticator']['Title'] = 'External Account';
+$lang['en_US']['FTP_Authenticator']['NoConnect'] = 'Could not connect to FTP server';
+$lang['en_US']['IMAP_Authenticator']['Protocol'] = 'Protocol is not set to a valid type';
+$lang['en_US']['LDAP_Authenticator']['NotConnected'] = 'Failed to connect to LDAP server.';
+$lang['en_US']['LDAP_Authenticator']['Version'] = 'Set LDAP protocol version to %d failed';
+$lang['en_US']['LDAP_Authenticator']['TLS'] = 'Start TLS failed: [%d] %s';
+$lang['en_US']['LDAP_Authenticator']['NoBind'] = 'Could not bind to LDAP server.';
+$lang['en_US']['LDAP_Authenticator']['NoPasswd'] = 'Please enter a password';
+$lang['en_US']['LDAP_Authenticator']['Expired'] = 'Your password has expired';
+$lang['en_US']['LDAP_Authenticator']['WillExpire'] = 'Your password expires in %d days';
+
+?>
---
+++ lang/nl_NL.php
@@ -0,0 +1,18 @@
+<?php
+
+global $lang;
+
+$lang['en_US']['ExternalAuthenticator']['EnterUID'] = 'Voer a.u.b. een  %s in';
+$lang['en_US']['ExternalAuthenticator']['Failed'] = 'Authenticatie mislukt';
+$lang['en_US']['ExternalAuthenticator']['Title'] = 'Extern Account';
+$lang['en_US']['FTP_Authenticator']['NoConnect'] = 'Kan geen verbinding maken met FTP server';
+$lang['en_US']['IMAP_Authenticator']['Protocol'] = 'Het protocol is niet ingesteld op een bekend type';
+$lang['en_US']['LDAP_Authenticator']['NotConnected'] = 'Kan geen verbinding maken met LDAP server.';
+$lang['en_US']['LDAP_Authenticator']['Version'] = 'Kan het LDAP protocol niet instellen op versie %d';
+$lang['en_US']['LDAP_Authenticator']['TLS'] = 'Starten van TLS sessie mislukt: [%d] %s';
+$lang['en_US']['LDAP_Authenticator']['NoBind'] = 'Kan niet binden aan de LDAP server.';
+$lang['en_US']['LDAP_Authenticator']['NoPasswd'] = 'Voer a.u.b. een wachtwoord in';
+$lang['en_US']['LDAP_Authenticator']['Expired'] = 'Uw wachtwoord is verlopen';
+$lang['en_US']['LDAP_Authenticator']['WillExpire'] = 'Uw wachtwoord zal verlopen over %d dagen';
+
+?>
