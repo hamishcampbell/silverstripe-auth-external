7df49fd3c4ac3f59d052b8dd460547b52cd39648
miiihi <blaz@innovatif.com>
2011-03-22 12:26:52 +0100

    Added sl_SI translation.

---
+++ lang/sl_SI.php
@@ -0,0 +1,31 @@
+<?php
+
+global $lang;
+
+$lang['sl_SI']['ExternalAuthenticator']['ModFormHead'] = 'ID za zunanjo avtentikacijo vira';
+$lang['sl_SI']['ExternalAuthenticator']['EnterUser'] = 'ID-uporabnika in vir za avtentikacijo tega uporabnika';
+$lang['sl_SI']['ExternalAuthenticator']['EnterNewId'] = 'ID mora biti uporabljen s tem virom';
+$lang['sl_SI']['ExternalAuthenticator']['UserExists'] = 'Uporabnik s tem uporabniškim imenom že obstaja';
+$lang['sl_SI']['ExternalAuthenticator']['Failed'] = 'Avtentikacija ni uspela';
+$lang['sl_SI']['ExternalAuthenticator']['EnterUID'] = 'Vpišite %s';
+$lang['sl_SI']['ExternalAuthenticator']['MailAddr'] = 'e-naslov';
+$lang['sl_SI']['ExternalAuthenticator']['EnterMailAddr'] = 'Vpišite e-naslov';
+$lang['sl_SI']['ExternalAuthenticator']['GroupExists'] = 'Uporabniški račun s tem e-naslovom že obstaja';
+$lang['sl_SI']['ExternalAuthenticator']['Title'] = 'Zunanji dostop';
+$lang['sl_SI']['ExternalAuthenticator']['LogOutIn'] = 'Prijavite se z drugim uporabniškim računom';
+$lang['sl_SI']['ExternalAuthenticator']['Password'] = 'Geslo';
+$lang['sl_SI']['ExternalAuthenticator']['Remember'] = 'Zapomni si me na tem računalniku';
+$lang['sl_SI']['ExternalAuthenticator']['Sources'] = 'Avtentikacija virov';
+$lang['sl_SI']['ExternalAuthenticator']['Login'] = 'Prijava';
+$lang['sl_SI']['ExternalAuthenticator']['LogFailed'] = 'Dostop do poročil o odpravi napak ni možen';
+$lang['sl_SI']['FTP_Authenticator']['NoConnect'] = 'Nimam povezave s FTP-strežnikom';
+$lang['sl_SI']['IMAP_Authenticator']['Protocol'] = 'Protokol ni ustrezno nastavljen';
+$lang['sl_SI']['LDAP_Authenticator']['NotConnected'] = 'Nimam povezave z LDAP-strežnikom (not connected).';
+$lang['sl_SI']['LDAP_Authenticator']['Version'] = 'Nastavljanje LDAP-protokola na različico %d ni uspelo';
+$lang['sl_SI']['LDAP_Authenticator']['TLS'] = 'Poganjanje TLS ni uspelo: [%d] %s';
+$lang['sl_SI']['LDAP_Authenticator']['NoBind'] = 'Nimam povezave z LDAP-strežnikom (no bind).';
+$lang['sl_SI']['LDAP_Authenticator']['NoPasswd'] = 'Vpišite geslo';
+$lang['sl_SI']['LDAP_Authenticator']['Expired'] = 'Vaše geslo je poteklo';
+$lang['sl_SI']['LDAP_Authenticator']['WillExpire'] = 'Vaše geslo bo poteklo v %d dneh';
+
+?>
