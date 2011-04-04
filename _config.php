03697af3f00317cb8a420ff324e0269918c06cfd
Hamish Campbell <hn.campbell@gmail.com>
2011-04-04 16:05:10 +1200

    Removed baked in config. Allows others to keep sources current easily. Closes: #2.

--- _config.php
+++ _config.php
@@ -2,6 +2,3 @@
 /**
  * See the docs directory for configuration examples
  **/
-Authenticator::register_authenticator('ExternalAuthenticator');
-ExternalAuthenticator::setValidAddress('silverstripe','127.0.0.1');
-
